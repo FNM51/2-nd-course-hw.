5fb3f45164ddcae6cae60ed1fa8e1db427b80a2a
Fnami51 <fnm5160@gmail.com>
2024-01-09 16:50:51 +0200

    pravka

--- hw-2.5.js
+++ hw-2.5.js
@@ -16,20 +16,19 @@ console.log(findLess(6, 6))
 
 //Task #2
 function parity(number) {
-    let type = Boolean(number);
-    if (type) {
+    if (!isNaN(number)) {
         if (number % 2 === 0) {
-            alert('Число четное')
+            return ('Число четное');
         } else {
-            alert('Число нечетное')
+            return ('Число нечетное');
         }
     } else {
-        alert('Это не число :-(')
+        return ('Это не число :-(');
     }
 }
 
 let userNumber = Number(prompt('Пожалуйста, введите любое число'));
-parity(userNumber);
+alert(parity(userNumber));
 
 //Task #3
 const rootInConsole = (number) => console.log(Math.sqrt(number));
@@ -54,47 +53,47 @@ checkUserAge()
 //Task #5
 function multiTwoNumbers(number1, number2) {
     if (isNaN(number1) && isNaN(number2)) {
-        alert('Одно или оба значения не являются числом');
+        return ('Одно или оба значения не являются числом');
     } else {
         let result = Number(number1) * Number(number2);
-        alert(`Произведение твоих чисел равно ${result}`)
+        return (`Произведение твоих чисел равно ${result}`)
     }
 }
 let number1 = prompt("Введи первое число");
 let number2 = prompt("Введи второе число");
-multiTwoNumbers(number1, number2);
+alert(multiTwoNumbers(number1, number2));
 
 //Task #6
 function cube() {
     let userNumber = prompt("Напиши число")
     if (isNaN(userNumber)) {
-        alert('Переданный параметр не является числом')
+        return ('Переданный параметр не является числом')
     } else {
-        alert(`${userNumber} в кубе равняется ${Number(userNumber) ** 2}`)
+        return (`${userNumber} в кубе равняется ${Number(userNumber) ** 2}`)
     }
 };
-cube(); 
+alert(cube()); 
 
 //Task #7
 const circle1 = {
     radius : 12,
     getArea() {
-        console.log(this.radius * Math.PI ** 2);
+        return (this.radius * Math.PI ** 2);
     },
     getPerimeter() {
-        console.log(this.radius * Math.PI * 2);
+        return (this.radius * Math.PI * 2);
     }
 }
 const circle2 = {
     radius : 16,
     getArea() {
-        console.log(this.radius * Math.PI ** 2);
+        return (this.radius * Math.PI ** 2);
     },
     getPerimeter() {
-        console.log(this.radius * Math.PI * 2);
+        return (this.radius * Math.PI * 2);
     }
 }
-circle1.getArea();
-circle1.getPerimeter();
-circle2.getArea();
-circle2.getPerimeter();
+console.log(circle1.getArea());
+console.log(circle1.getPerimeter());
+console.log(circle2.getArea());
+console.log(circle2.getPerimeter());
