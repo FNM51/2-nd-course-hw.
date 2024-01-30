2f6e1e709ae2fc8866e8b295f29c158aa30411c0
Fnami51 <fnm5160@gmail.com>
2024-01-30 16:59:34 +0200

    hw-2.8

---
+++ hw-2.8.js
@@ -0,0 +1,88 @@
+//Task-1
+function task1(){
+    const people = [
+       { name: 'Глеб', age: 29 },
+       { name: 'Анна', age: 17 },
+       { name: 'Олег', age: 7 },
+       { name: 'Оксана', age: 47 }
+    ];
+  
+    console.log(people.sort(function (a, b){
+      return a.age - b.age;
+    }));
+  }
+  
+  //Task-2
+  function isPositive(number) {
+    return number > 0;
+  }
+  function isMale(element) {
+    if (element.gender == 'male') {
+      return element;
+    }
+  }
+  function filter(array, ruleFunction) {
+    const output = [];
+    for (let i = 0; i < array.length; i++){
+      if (ruleFunction(array[i])) {
+        output.push(array[i]);
+      }
+    }
+    return output;
+  }
+  function task2(){
+    console.log(filter([3, -4, 1, 9], isPositive));
+  
+    const people = [
+       {name: 'Глеб', gender: 'male'},
+       {name: 'Анна', gender: 'female'},
+       {name: 'Олег', gender: 'male'},
+       {name: 'Оксана', gender: 'female'}
+    ];
+  
+    console.log(filter(people, isMale));
+  }
+  
+  //Task-3
+  function task3() {
+    const timer = (seconds) => {
+      let myDate = new Date();
+      const interval = setInterval(() => {
+          console.log(myDate);
+      }, 3000);
+  
+      setTimeout(() => {
+          clearInterval(interval);
+          console.log(`${seconds} секунд прошло!`)
+      }, seconds * 1000)
+    };
+  
+    const seconds = 30;
+    timer(seconds);
+  }
+  
+  //Task-4
+  function task4(){
+    function delayForSecond(callback) {
+      setTimeout(callback, 1000)
+      callback();
+    }
+  
+    delayForSecond(function () {
+       console.log('Привет, Глеб!');
+    })
+  }
+  
+  //Task-5
+  function delayForSecond(callback) {
+      setTimeout(() => {
+          console.log('Прошла одна секунда');
+          if(callback) { 	callback(); }
+      }, 1000)
+  }
+  function sayHi (name) {
+      console.log(`Привет, ${name}!`);
+  }
+  function task5(){
+    delayForSecond(() => sayHi('Глеб'))
+  }
